3c0f53bd4eeacab6ef337008cf4d23d2459cce59
Arjan Gupta <arjangupta95@gmail.com>
2022-04-02 00:24:50 -0500

    Reassign a column

--- week2/data-manip.m
+++ week2/data-manip.m
@@ -1,4 +1,4 @@
-A = [1,2,3; 4,5,6; 7,8,9];
+A = [1,2,3; 4,5,6; 7,8,9]
 
 % Get all elements in second row of A
 A(2,:)
@@ -8,3 +8,7 @@ A(:,3)
 
 % Get all elems in first and second row of A
 A([1,2],:)
+
+% Reassign a column in A
+A(:,2) = [120, 150, 180];
+A
